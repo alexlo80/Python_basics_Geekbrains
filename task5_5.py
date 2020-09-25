fadf48ca16a12971c9edbcee9751ec9f9a98003f
alexlo80 <alexlo80@mail.ru>
2020-09-25 15:11:33 +0300

    Update task5_5.py

--- task5_5.py
+++ task5_5.py
@@ -4,9 +4,9 @@
 import random
 import re
 
-with open("text2.txt", "a", encoding='UTF-8') as f_obj:
+with open("text2.txt", "w", encoding='UTF-8') as f_obj:
     f_obj.writelines(str(random.randint(1, 100)) + ' ' for _ in range(10))  # вводим 10 цифр до 100
-with open("text2.txt", encoding='UTF-8') as sum_chisel:
+with open("text2.txt", "r", encoding='UTF-8') as sum_chisel:
     lis_avg = [re.split(' |\n', line.rstrip())for line in sum_chisel]
     sum_chis = [int(num) for num in lis_avg[0]]
     print("Были введены такие цифры: ", *lis_avg)
